d02d708c1987ec9464a93f70e8e13656a31a3db6
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-20 12:22:16 +0300

    installed pusher kit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+    import ChatMessage from './components/ChatMessage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    class App extends Component {
+      constructor(props) {
+          super(props);
+          this.state = {
+            currentView: 'ChatMessage'
+          }
+          this.changeView = this.changeView.bind(this);
+      }
 
-export default App;
+      changeView(view) {
+          this.setState({
+              currentView: view
+          })
+      }
+
+      render() {
+            let view ='';
+            if (this.state.currentView === "ChatMessage") {
+                view = <ChatMessage  changeView={this.changeView}/>
+            }
+            return (
+                <div className="App">
+                    {view}
+                </div>
+            );
+        }
+    }
+    export default App;
